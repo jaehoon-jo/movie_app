eec201136ff1c03c74fca972f3bf05eb56b309d6
jaehoon-jo <chojh922@gmail.com>
2020-11-10 17:36:12 +0900

    Updates

---
+++ precache-manifest.8c1956de80bc4ecf7002a68136a8ce8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c34094c4452e71dc7395838d638b9a88",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "9253c1071bdd02ae7374",
+    "url": "/movie_app/static/css/main.30e5db8e.chunk.css"
+  },
+  {
+    "revision": "fa5e0544dd32d30ca5a4",
+    "url": "/movie_app/static/js/2.9b6f4c02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.9b6f4c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9253c1071bdd02ae7374",
+    "url": "/movie_app/static/js/main.f255128e.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
